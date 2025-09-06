a43829a2e02e5bdd819a9005efbd7055a55dbd5c
Mohammad Shan <mohdshan1024@gmail.com>
2025-09-06 10:22:04 +0530

    Header component added

---
+++ expensio/components/Header.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import { Shield } from "lucide-react";
+import Link from "next/link";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+  SignInButton,
+} from "@clerk/clerk-react";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const pathName = usePathname();
+  const isHomePage = pathName === "/";
+  return (
+    <div
+      className={`p-4 flex justify-between items-center ${
+        isHomePage ? "bg-blue-50" : "bg-white border-b border-blue-50"
+      }`}
+    >
+      <Link href="/" className="flex items-center">
+        <Shield className="w-6 h-6 text-blue-600 mr-2" />
+
+        <h1 className="text-xl font-semibold">Expensio</h1>
+      </Link>
+
+      <div className="flex items-center space-x-4">
+        <SignedIn>
+          <Link href={"/receipts"}>
+            <Button variant={"outline"}>My Receipts</Button>
+          </Link>
+
+          <Link href={"/manage-plan"}>
+            <Button>Manage Plan</Button>
+          </Link>
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Login</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
